8e5a9da9a54e63a76387ac3d15eb5c3b6354ac79
tkinopi <changmuxia76@gmail.com>
2025-08-10 21:25:24 +0900

    web事業のページ変更

--- client/src/pages/services/web.tsx
+++ client/src/pages/services/web.tsx
@@ -141,7 +141,7 @@ export default function WebPage() {
         </section>
 
         {/* Technology Stack */}
-        <section className="py-20 bg-gray-50">
+        {/* <section className="py-20 bg-gray-50">
           <div className="container mx-auto px-4">
             <div className="max-w-6xl mx-auto">
               <motion.div
@@ -212,10 +212,10 @@ export default function WebPage() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         {/* Project Types */}
-        <section className="py-20 bg-white">
+        {/* <section className="py-20 bg-white">
           <div className="container mx-auto px-4">
             <div className="max-w-4xl mx-auto">
               <motion.div
@@ -303,10 +303,10 @@ export default function WebPage() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         {/* Pricing */}
-        <section className="py-20 bg-gray-50">
+        {/* <section className="py-20 bg-gray-50">
           <div className="container mx-auto px-4">
             <div className="max-w-4xl mx-auto">
               <motion.div
@@ -405,7 +405,7 @@ export default function WebPage() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
         {/* CTA Section */}
         <section className="py-20 bg-pink-500 text-white">
