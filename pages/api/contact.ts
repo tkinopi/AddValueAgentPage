694248d7edcc88715d1aee8b5b40e2e660069f31
tkinopi <changmuxia76@gmail.com>
2025-08-11 23:33:49 +0900

    ビルドエラー解消

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -16,7 +16,7 @@ type ContactResponse = {
 }
 
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.GMAIL_USER,
