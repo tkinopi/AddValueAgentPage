75c759b5b7048b6dec80d32c48b96c71037f9814
tkinopi <changmuxia76@gmail.com>
2025-08-11 20:14:50 +0900

    Restructure project for Amplify deployment - Simplify to standard Vite SPA structure

--- src/domain/repositories/IContactRepository.ts
+++
@@ -1,7 +0,0 @@
-import { ContactInquiry } from '../entities/ContactInquiry.js';
-
-export interface IContactRepository {
-  save(inquiry: ContactInquiry): Promise<ContactInquiry>;
-  findById(id: string): Promise<ContactInquiry | null>;
-  findAll(): Promise<ContactInquiry[]>;
-}
